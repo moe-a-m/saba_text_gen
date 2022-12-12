3c6dfef76b609a5c6b04a06dc6038337f5b1ab21
moe_a_m <mohammed.20m@gmail.com>
2022-12-12 18:13:12 +0200

    * add dataset loader and trainer * add score function to get answer score

--- model.py
+++ model.py
@@ -42,17 +42,21 @@ def get_prediction(text: str):
 
 def generate_text(question: str, keywords: str):
     answer = get_answer(question, keywords)
+    get_score(question, answer['score'])
     genText = get_prediction(answer['answer'])
     save_txt(question, genText)
     return genText
 
+
 def clean_filename(filename):
-    
+
     forbidden_chars = '"*\\/\'.|?:<>'
-    filename = ''.join([x if x not in forbidden_chars else '#' for x in filename])
+    filename = ''.join(
+        [x if x not in forbidden_chars else '#' for x in filename])
     if len(filename) >= 176:
         filename = filename[:170] + '...'
     return filename
+
 
 def save_txt(title, txt):
     dirName = 'answers'
@@ -62,14 +66,31 @@ def save_txt(title, txt):
         print("Directory ", dirName,  " Created ")
     else:
         print("Directory ", dirName,  " already exists")
-        
+
     title = clean_filename(title)
     current_dateTime = datetime.now()
-    current_time= "{:%B %d, %Y}".format(current_dateTime)
+    current_time = "{:%B %d, %Y}".format(current_dateTime)
     name = dirName+'/'+current_time+title+'.txt'
     with open(name, 'w', encoding='utf-8') as f:
 
         f.write(txt['generated_text'])
 
 
-# ans = generate_text("why do we learn","To have the option to get a job and elevate our self")
+def get_score(question, score):
+    dirName = 'score'
+    # Create target Directory if don't exist
+    if not os.path.exists(dirName):
+        os.mkdir(dirName)
+        print("Directory ", dirName,  " Created ")
+    else:
+        print("Directory ", dirName,  " already exists")
+        
+    question = clean_filename(question)
+    current_dateTime = datetime.now()
+    current_time= "{:%B %d, %Y}".format(current_dateTime)
+    name = dirName+'/'+current_time+question+'.txt'
+    
+    txt = question +', '+os.environ.get("ESSAY_LENGTH")+ ','+ str(score)
+    with open(name, 'w', encoding='utf-8') as f:
+
+        f.write(txt)
